dd59cd189cd80bd7c08ed40bb65a91c37d8cec82
Junilu Lacar <junilu.lacar@gmail.com>
2017-03-29 06:31:16 -0400

    Converted hard tabs to spaces

--- src/test/java/org/thymeleaf/util/NumberPointTypeTest.java
+++ src/test/java/org/thymeleaf/util/NumberPointTypeTest.java
@@ -14,27 +14,27 @@ import org.junit.Test;
 
 public class NumberPointTypeTest {
 
-	@Test
-	public void match_returns_null_when_invalid_enum_name() {
-		assertNull(NumberPointType.match("INVALID_NAME"));
-	}
-	
-	@Test
-	public void match_returns_valueOf_string() throws Exception {
-		assertEquals(POINT, match("POINT"));
-		assertEquals(COMMA, match("COMMA"));
-		assertEquals(WHITESPACE, match("WHITESPACE"));
-		assertEquals(NONE, match("NONE"));
-		assertEquals(DEFAULT, match("DEFAULT"));
-	}
-	
-	@Test
-	public void all_values_accounted_for_in_tests() throws Exception {
-		/* Developer Note: This test is intended to break if set of values of the 
-		 * enum type changes. If that happens, review the other tests here to make 
-		 * sure that they agree with the complete and current set of values.
-		 */
-		assertArrayEquals(new NumberPointType[] {POINT, COMMA, WHITESPACE, NONE, DEFAULT}, 
-				NumberPointType.values());
-	}
+    @Test
+    public void match_returns_null_when_invalid_enum_name() {
+        assertNull(NumberPointType.match("INVALID_NAME"));
+    }
+    
+    @Test
+    public void match_returns_valueOf_string() throws Exception {
+        assertEquals(POINT, match("POINT"));
+        assertEquals(COMMA, match("COMMA"));
+        assertEquals(WHITESPACE, match("WHITESPACE"));
+        assertEquals(NONE, match("NONE"));
+        assertEquals(DEFAULT, match("DEFAULT"));
+    }
+    
+    @Test
+    public void all_values_accounted_for_in_tests() throws Exception {
+        /* Developer Note: This test is intended to break if set of values of the 
+         * enum type changes. If that happens, review the other tests here to make 
+         * sure that they agree with the complete and current set of values.
+         */
+        assertArrayEquals(new NumberPointType[] {POINT, COMMA, WHITESPACE, NONE, DEFAULT}, 
+                NumberPointType.values());
+    }
 }
