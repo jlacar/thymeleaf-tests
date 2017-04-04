91d43f7f15365d25c55c436635ca2b968978b4ff
Junilu Lacar <junilu.lacar@gmail.com>
2017-04-04 00:44:11 -0400

    Added JavaDocs and refactored some constant declarations

--- src/test/java/org/thymeleaf/standard/expression/AbstractExpressionTest.java
+++ src/test/java/org/thymeleaf/standard/expression/AbstractExpressionTest.java
@@ -13,10 +13,14 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
  * Abstract base class for testing expressions in a dialect.
  * <p>
  * This class provides the boilerplate code for testing expressions 
- * in a dialect. Subclasses should implement the getTemplateEngine()
- * method and the 
+ * in a dialect. Subclasses should override the {@link #createTemplateEngine}
+ * method to return the appropriate template engine to use.
+ * 
+ * @see TemplateEngine
+ * @see IMessageResolver
+ * @see ITemplateResolver
  *  
- * @author jlacar
+ * @author Junilu Lacar
  */
 public abstract class AbstractExpressionTest {
 
@@ -31,26 +35,66 @@ public abstract class AbstractExpressionTest {
         this.templateEngine.setMessageResolver(messageResolver());
     }
 
+    /**
+     * Evaluates the given expression using the context provided
+     * by {@link #defaultContext()}.
+     * 
+     * @param expression    the expression to evaluate
+     * @return  the result of processing the expression with this
+     *          test's template engine and default context
+     */
     protected final String eval(final String expression) {
         return eval(expression, defaultContext());
     }
     
+    /**
+     * Evaluates the given expression using the given context.
+     * 
+     * @param expression    the expression to evaluate
+     * @param context       the context to use when processing the expression
+     * @return  the result of processing the expression with this
+     *          test's template engine and the given context
+     */
     protected final String eval(final String expression, IContext context) {
         return extractResult(this.templateEngine.process(expression, context));
     }
     
+    /***
+     * Returns the template engine used by this test.
+     * 
+     * @return  the template engine used by this test
+     */
     protected final TemplateEngine templateEngine() {
         return this.templateEngine;
     }
     
+    /**
+     * Returns the context to use with {@link #eval(String)}.
+     * Override this method if you want to use a different 
+     * context instead. 
+     * 
+     * @return  the context to use if one is not specified 
+     *          for evaluating an expression
+     */
     protected IContext defaultContext() {
         return DEFAULT_CONTEXT;
     }
     
     /**
+     * Extracts the result that this test's assertions are going
+     * to be checking. Defaults to the entire processed template.
+     * <p>
+     * Override this method to customize what the {@link #eval} method 
+     * returns. You may want to do this if you're only interested in
+     * checking the part or parts of the processed template that was 
+     * changed by the template engine, thus ignoring any of the static 
+     * boilerplate parts and making test assertions cleaner and clearer.
      * 
-     * @param processedTemplate
-     * @return
+     * @param processedTemplate     the result of processing this test's
+     *                              template with its template engine
+     *                              
+     * @return  the part of the processed template that this test's 
+     *          assertions are going to check 
      */
     protected String extractResult(String processedTemplate) {
         return processedTemplate;
@@ -68,7 +112,7 @@ public abstract class AbstractExpressionTest {
     }
     
     /**
-     * Returns the ITemplateResolver implementation to use with
+     * Returns the {@link ITemplateResolver} implementation to use with
      * the template engine in this test.
      * 
      * @return the template resolver to use
@@ -76,7 +120,7 @@ public abstract class AbstractExpressionTest {
     protected abstract ITemplateResolver templateResolver();
     
     /**
-     * Returns the IMessageResolver implementation to use with
+     * Returns the {@link IMessageResolver} implementation to use with
      * the template engine in this test.
      * 
      * @return the message resolver to use
--- src/test/java/org/thymeleaf/standard/expression/BaseStandardDialectExpressionTest.java
+++ src/test/java/org/thymeleaf/standard/expression/BaseStandardDialectExpressionTest.java
@@ -6,32 +6,91 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.messageresolver.IMessageResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+/**
+ * Provides the boilerplate parts of the template that will be used to
+ * test expressions in the Thymeleaf Standard Dialect.
+ * <p>
+ * This implementation overrides the {@link #extractResult} method to
+ * return only the part of the result of processed template that represents
+ * the value of an expression after it has been processed by the template
+ * engine.
+ * <p>
+ * Subclasses can override the {@link #messageResolver()} and {@link #messageResolverProperties()}
+ * methods to specify custom messages.  
+ * 
+ * @author Junilu Lacar
+ */
 public class BaseStandardDialectExpressionTest extends AbstractExpressionTest {
 
-    private static final String TEMPLATE = "<!DOCTYPE html><html><body><span th:text=\"{%%}\">PLACEHOLDER</span></body></html>";
+    
     private static final String RESULT_PREFIX = "<!DOCTYPE html><html><body><span>";
     private static final String RESULT_SUFFIX = "</span></body></html>";
     
+    private static final String TEMPLATE = "<!DOCTYPE html>" +
+            "<html><body><span th:text=\"{%%}\">PLACEHOLDER" + RESULT_SUFFIX;;
+    
+    /**
+     * Creates the template engine that will bu used in this test.
+     * <p>
+     * This implementation creates an instance of {@link TemplateEngine}.
+     * 
+     * @return the template engine that will be used in this test
+     */
     @Override
     protected final TemplateEngine createTemplateEngine() {
         return super.createTemplateEngine();
     }
     
+    /**
+     * Takes the given result of processing a template and extracts the 
+     * value that this test's assertions are going to be checking.
+     * <p>
+     * This implementation extracts only the part that corresponds to
+     * the result of evaluating an expression in the Standard Dialect.
+     * 
+     * @return the value that will be checked by this test's assertions
+     */
     @Override
     protected final String extractResult(String processedTemplate) {
-        return processedTemplate.substring(RESULT_PREFIX.length(), processedTemplate.length() - RESULT_SUFFIX.length());
+        return processedTemplate.substring(RESULT_PREFIX.length(), 
+                processedTemplate.length() - RESULT_SUFFIX.length());
     }
     
+    /**
+     * Returns the template resolver that will be used by this test's
+     * template engine.
+     * <p>
+     * This implementation returns an instance of {@link TestTemplateResolver}  
+     * that has been configured with a minimal template that includes a placeholder 
+     * for one Standard Dialect expression.
+     */
     @Override
     protected final ITemplateResolver templateResolver() {
         return new TestTemplateResolver(TEMPLATE);
     }
 
+    /**
+     * Returns the message resolver that will be used by this test's
+     * template engine.
+     * <p>
+     * This implementation returns an instance of {@link TestMessageResolver}
+     * that has been configured with the properties that are provided by
+     * {@link #messageResolverProperties()}.
+     * 
+     * @return the message resolver to use with this test's template engine
+     */
     @Override
     protected IMessageResolver messageResolver() {
         return new TestMessageResolver(messageResolverProperties());
     }
 
+    /**
+     * Specifies the {@link java.util.Properties} that the {@link IMessageResolver}
+     * implementation used by this test will use. Override this method to customize
+     * the message properties used in this test.
+     * 
+     * @return the message properties used in this test
+     */
     protected Properties messageResolverProperties() {
         return new Properties();
     }
